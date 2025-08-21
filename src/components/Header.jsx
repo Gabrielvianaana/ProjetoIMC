a52bd1e401950a408aed9700433e49061b0783a2
gabriel <gabrielviana1939@icloud.com>
2025-08-21 09:11:37 -0300

    criando a estrutura de codigo dos componentes Header e Resultado

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import '../css/global.css'
+import '../css/header.css'
+function Header(){
+    return(
+        <div className="header">
+        <h1>calculadora IMC</h1>
+        </div>
+    )
+}
+export default Header
